9a6086c67ac5d8150d17b2c9e76715ce23d31c07
Haoyu <arhyrhy185@gmail.com>
2020-06-14 09:13:15 +0200

    change temperature from integer to random floating number

--- test3.cpp
+++ test3.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 
 using std::vector;
 
@@ -11,10 +12,11 @@ double calculateStd(vector<vector<double>>& data) {
         std::cout << "No data avaiable!" << std::endl;
         return 0;
     }
-    double dAvg = 0;
+    double dMean = 0;
     double dVariance = 0;
-    dAvg = std::accumulate(data.begin(), data.end(), 0, [](double sum, vector<double>& rec) {return sum + rec[0]; }) / data.size();
-    dVariance = std::accumulate(data.begin(), data.end(), 0, [dAvg](double sum, vector<double>& rec) { return sum + pow(rec[0] - dAvg, 2); }) / data.size();
+    dMean = std::accumulate(data.begin(), data.end(), 0, [](double sum, vector<double>& rec) {return sum + rec[0]; }) / data.size();
+    // take each number, subtract the mean and square the result
+    dVariance = std::accumulate(data.begin(), data.end(), 0, [dMean](double sum, vector<double>& rec) { return sum + pow(rec[0] - dMean, 2); }) / data.size();
     return sqrt(dVariance);
 }
 
@@ -23,25 +25,29 @@ int main()
     // random range [m, m+n[ is set here
     int n = 40, m = 1;
     // The length of dataset
-    int times = 10;
+    int times = 50;
     // Dataset
     vector<vector<double>> data;
 
     // Random seed
     srand(time(nullptr));
 
-    for (int i = 0; i < times; i++) {
-        // Random number between [m, m+n[
-        vector<double> temp = { (double)(rand() % n + m) , (double)i};
+    for (size_t i = 0; i < times; ++i) {
+        // Random floating number between [m, m+n[
+        vector<double> temp = { (double)(rand() % n + m + rand() / (RAND_MAX + 1.) - 0.5), (double)i};
         data.push_back(temp);
     }
 
     double dStd = calculateStd(data);
 
+    // Set output decimal place
+    std::cout << std::fixed; 
+    std::cout << std::setprecision(2);
+
     for (const auto& rec : data) {
         std::cout << "Time: " << rec[1] + 1 << "  Temperature: " << rec[0] << std::endl;
     }
 
-    std::cout << "Std: " << dStd << std::endl;
+    std::cout << "The Standard Deviation: " << dStd << std::endl;
 }
 
